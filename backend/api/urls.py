d8213805a1007528edf554b1a96ac901555643e1
Ольга Шполянская <olgab497@gmail.com>
2023-08-20 22:51:50 +0300

    Correction flake8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,45 +2,50 @@ from django.urls import include, path
 from djoser.views import TokenDestroyView, UserViewSet
 from rest_framework.routers import DefaultRouter
 
-from api.views import (AddDeleteFavoriteRecipe, AddDeleteShoppingCart,
-                       AddDeleteSubscription, GetToken, IngredientsViewSet,
-                       RecipesViewSet, TagsViewSet, UsersViewSet)
+from api.views import (
+    AddDeleteFavoriteRecipe,
+    AddDeleteShoppingCart,
+    AddDeleteSubscription,
+    GetToken,
+    IngredientsViewSet,
+    RecipesViewSet,
+    TagsViewSet,
+    UsersViewSet,
+)
 
-app_name = 'api'
+app_name = "api"
 
 router = DefaultRouter()
-router.register(r'users', UsersViewSet)
-router.register(r'tags', TagsViewSet)
-router.register(r'ingredients', IngredientsViewSet)
-router.register(r'recipes', RecipesViewSet)
+router.register(r"users", UsersViewSet)
+router.register(r"tags", TagsViewSet)
+router.register(r"ingredients", IngredientsViewSet)
+router.register(r"recipes", RecipesViewSet)
 
 
 urlpatterns = [
-     path(
-          'auth/token/login/',
-          GetToken.as_view(),
-          name='login'),
-     path(
-         'logout/',
-         TokenDestroyView.as_view(),
-         name='token_destroy'),
-     path(
-         'users/set_password/',
-         UserViewSet.as_view({'post': 'set_password'}),
-         name='set_password'),
-     path(
-          'users/<int:user_id>/subscribe/',
-          AddDeleteSubscription.as_view(),
-          name='subscribe'),
-     path(
-          'recipes/<int:recipe_id>/favorite/',
-          AddDeleteFavoriteRecipe.as_view(),
-          name='favorite_recipe'),
-     path(
-          'recipes/<int:recipe_id>/shopping_cart/',
-          AddDeleteShoppingCart.as_view(),
-          name='shopping_cart'),
-     path('', include(router.urls)),
-     path('auth/', include('djoser.urls')),
-     path('auth/', include('djoser.urls.authtoken')),
+    path("auth/token/login/", GetToken.as_view(), name="login"),
+    path("logout/", TokenDestroyView.as_view(), name="token_destroy"),
+    path(
+        "users/set_password/",
+        UserViewSet.as_view({"post": "set_password"}),
+        name="set_password",
+    ),
+    path(
+        "users/<int:user_id>/subscribe/",
+        AddDeleteSubscription.as_view(),
+        name="subscribe",
+    ),
+    path(
+        "recipes/<int:recipe_id>/favorite/",
+        AddDeleteFavoriteRecipe.as_view(),
+        name="favorite_recipe",
+    ),
+    path(
+        "recipes/<int:recipe_id>/shopping_cart/",
+        AddDeleteShoppingCart.as_view(),
+        name="shopping_cart",
+    ),
+    path("", include(router.urls)),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,10 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api'))
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls", namespace="api")),
 ]
 
-urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
